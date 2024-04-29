513cdff3dfc496028ba2221fd4a76b2d51f48ee7
Ezgi Senyurt-Ertürk <156966522+ezgsnyrt@users.noreply.github.com>
2024-04-30 00:24:52 +0200

    add use effect to the function

--- src/components/Startpage.jsx
+++ src/components/Startpage.jsx
@@ -3,9 +3,14 @@ import Questions from "./Questions";
 import "../styles/intro.css";
 import "../styles/startpage.css";
 
+function StartPage(welcomeMessage) {
+    const [btnClicked, setBtnClicked] = useState(null);
 
-const [btnClicked, setBtnClicked] = useState(null);
+    const handleClick = () => {
+        setBtnClicked(1);
+    };
 
-const handleClick = () => {
-    setBtnClicked(1);
-};
+    useEffect(() => {
+        console.log("btnClicked: ", btnClicked)
+    }, [])
+}
