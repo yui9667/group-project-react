8592dda451b464c9b3dac5679b531becec08c45b
Yui Jensen <kyanyui67@gmail.com>
2024-05-08 16:32:18 +0200

    Updates component and package.json
    I updated my codes in component and added rollup in pacakge.josn

--- src/components/ShowAnswer.jsx
+++ src/components/ShowAnswer.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheck } from "@fortawesome/free-solid-svg-icons";
-import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
 import "../components/showanswer.css";
 
 const ShowAnswer = () => {
@@ -13,6 +13,8 @@ const ShowAnswer = () => {
   const [questionsAndAnswers, setQuestionsAndAnswers] = useState([]);
   // tracks the current question.  it starts at the first question (0)
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [answers, setAnswers] = useState([]);
+  // store each answer given by the user
   const [selectedAnswer, setSelectedAnswer] = useState();
 
   useEffect(() => {
@@ -60,9 +62,12 @@ const ShowAnswer = () => {
     // returns the shuffled array
     return array;
   };
+
   //Going to nextQuestions
   function nextQuestion() {
     if (currentQuestionIndex < 9) {
+      setLock(false);
+      setIcon(false);
       setCurrentQuestionIndex(currentQuestionIndex + 1);
     } else if (currentQuestionIndex === 9) {
       // setIsFinished(1);
@@ -72,8 +77,7 @@ const ShowAnswer = () => {
 
   //Showing correct or incorrect
   const checkAnswer = (e, ans) => {
-    const correctAnswer =
-      questionsAndAnswers[currentQuestionIndex].correctAnswer;
+    let correctAnswer = questionsAndAnswers[currentQuestionIndex].correctAnswer;
     //if lock is equal to false, change to true which means then user can select only one option.
     //if correct answer equal to ans, change the color to green, otherwise change to red.
     if (!lock) {
@@ -85,48 +89,101 @@ const ShowAnswer = () => {
         e.target.classList.add("wrong");
         setLock(true);
         setIcon("wrong");
+        //Showing correct answer when an user press the wrong
+        const correctBtn = document.querySelector(
+          `[data-answer="${correctAnswer}"]`
+        );
+
+        if (correctBtn) {
+          correctBtn.classList.add("correct");
+          correctBtn.innerHTML = `
+<FontAwesomeIcon
+icon={faCheck}
+size="sm"
+style={{ color: "#000000", paddingRight: "10px" }}
+/>
+    <span> ${correctAnswer}</span>
+  `;
+        }
+        console.log(correctBtn);
       }
 
       console.log("currentQuestionIndex:", currentQuestionIndex);
       console.log("correctAnswer:", correctAnswer);
-      console.log(ans);
+      // console.log(ans);
+      // push and store each answer to answer array
+
+      answers.push(ans);
       setSelectedAnswer(ans);
     }
   };
 
   return (
     //Adding optional chaining so if the {questionsAndAnswers[currentQuestionIndex] is null or undefined, accessing to the question
-    <div className="wrapper">
-      <h3>{questionsAndAnswers[currentQuestionIndex]?.question}</h3>
-      <div className="btn-container">
-        {questionsAndAnswers[currentQuestionIndex]?.shuffledAnswers.map(
-          (ans, index) => (
-            <button key={index} onClick={(e) => checkAnswer(e, ans)}>
-              {selectedAnswer === ans && icon === "correct" && (
-                <FontAwesomeIcon
-                  icon={faCheck}
-                  size="sm"
-                  style={{ color: "#000000", paddingRight: "10px" }}
-                />
-              )}
-              {selectedAnswer === ans && icon === "wrong" && (
-                <FontAwesomeIcon
-                  icon={faXmark}
-                  size="sm"
-                  style={{ color: "#000000", paddingRight: "10px" }}
-                />
-              )}
-              {String.fromCharCode(65 + index) + ". "}
-              {ans}
-            </button>
-          )
-        )}
+    //Use dangerouslySetInnerHTML for removing special characters in the questions. Because of the code structure of buttons which include children, this feature could not be included in buttons.
+    //Use ternary condition aligning with react.fragment in to let the first question be seen before the button next and to have a link to the result page
+    <React.Fragment>
+      <div className="body">
+        <h2 className="currentQuestionTrack">
+          Question {currentQuestionIndex + 1} / 10
+        </h2>
+        {questionsAndAnswers.length > 0 ? (
+          <div className="wrapper">
+            <h3
+              className="currentQuestion"
+              dangerouslySetInnerHTML={{
+                __html: questionsAndAnswers[currentQuestionIndex]?.question,
+              }}
+            ></h3>
 
-        <div className="controls">
-          <button className="next-btn">Next Question</button>
-        </div>
+            <div className="btn-container">
+              {questionsAndAnswers[currentQuestionIndex]?.shuffledAnswers.map(
+                (ans, index) => (
+                  // when key = index, it only render the 4 options once. but we need to change (re-render) the all elements, so create a unique button using unique key value including #question. This also provides the syle being reset.
+                  <button
+                    className="btn"
+                    key={currentQuestionIndex + "-" + index}
+                    onClick={(e) => checkAnswer(e, ans)}
+                    data-answer={ans}
+                  >
+                    {selectedAnswer === ans && icon === "correct" && (
+                      <FontAwesomeIcon
+                        icon={faCheck}
+                        size="sm"
+                        style={{ color: "#000000", paddingRight: "10px" }}
+                      />
+                    )}
+
+                    {selectedAnswer === ans && icon === "wrong" && (
+                      <FontAwesomeIcon
+                        icon={faXmark}
+                        size="sm"
+                        style={{ color: "#000000", paddingRight: "10px" }}
+                      />
+                    )}
+
+                    {index + 1 + ". "}
+
+                    {ans}
+                  </button>
+                )
+              )}
+            </div>
+            <div className="controls">
+              <button onClick={nextQuestion} className="next-btn">
+                Next Question
+              </button>
+              {
+                // After the last page is completed, 'ResultPage' will be updated considering the given name to move on to the last page.
+                questionsAndAnswers.length === 9 ? <ResultPage /> : ""
+              }
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
--- src/components/Startpage.jsx
+++ src/components/Startpage.jsx
@@ -4,36 +4,36 @@ import "../styles/intro.css";
 import "../styles/startpage.css";
 
 function StartPage(welcomeMessage) {
-    const [btnClicked, setBtnClicked] = useState(null);
+  const [btnClicked, setBtnClicked] = useState(null);
 
-    const handleClick = () => {
-        setBtnClicked(1);
-    };
+  const handleClick = () => {
+    setBtnClicked(1);
+  };
 
-    useEffect(() => {
-        console.log("btnClicked: ", btnClicked)
-    }, [])
+  useEffect(() => {
+    console.log("btnClicked: ", btnClicked);
+  }, []);
 
-    return (
-        <div className="container">
-            <h2>QUIZ TIME</h2>
-            <div className="start-container">
-                {
-                btnClicked == null && (
-                    <div className="welcome-container">
-                        <p className="welcome-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic beatae quas officia a nihil, asperiores impedit fugit aliquam ex dolorem. Autem inventore, nam tempore recusandae velit eos!</p>
-                        <button onClick={handleClick} className="btn btn-primary start-btn">Start</button>
-                    </div>
-                    )
-                }
-                {
-                    btnClicked !== null && (
-                        <Questions />
-                    )
-                }
-            </div>
-        </div>
-    )
+  return (
+    <div className="container">
+      <h2>QUIZ TIME</h2>
+      <div className="start-container">
+        {btnClicked == null && (
+          <div className="welcome-container">
+            <p className="welcome-text">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic
+              beatae quas officia a nihil, asperiores impedit fugit aliquam ex
+              dolorem. Autem inventore, nam tempore recusandae velit eos!
+            </p>
+            <button onClick={handleClick} className="btn btn-primary start-btn">
+              Start
+            </button>
+          </div>
+        )}
+        {btnClicked !== null && <Questions />}
+      </div>
+    </div>
+  );
 }
 
 export default StartPage;
