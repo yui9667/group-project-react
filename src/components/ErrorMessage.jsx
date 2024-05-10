a7b76fc8d0cc560123412385222bab8a17faf937
Daniela Y. Zagal <149149177+danielaayz@users.noreply.github.com>
2024-05-10 10:18:35 +0200

    feat: new error modal

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,9 +1,47 @@
+import { useState } from "react";
+
 const ErrorMessage = () => {
+   const [errorMessage, setErrorMessage] = useState(false);
+
+   const toggleModal = () => {
+      setErrorMessage(!errorMessage); //! = true to false and false to true
+   };
+
+   const backToMenu = () => {
+      console.log("to do back to menu");
+   };
+
+   if (errorMessage) {
+      document.body.classList.add("active-modal");
+   } else {
+      document.body.classList.remove("active-modal");
+   }
+
+   const element = <FontAwesomeIcon icon={faXmark} />;
+
    return (
-      <div className={"error-page"}>
-         <div className={"oops"}>Oops!</div>
-         <div className={"message"}>Something went wrong...</div>
-      </div>
+      <>
+         {errorMessage && (
+            <div className="modal">
+               <div onClick={toggleModal} className="overlay"></div>
+               <div className="popup-content">
+                  <div onClick={toggleModal} className="xmark">
+                     <FontAwesomeIcon icon={faXmark} size="2x" />
+                  </div>
+                  <h3 className="modal-info">
+                     Something went wrong. We couldn't start the game. Please
+                     try starting it again.{" "}
+                  </h3>
+                  <div className="buttons">
+                     <button onClick={backToMenu} className="exit-btn">
+                        {" "}
+                        Start the game again
+                     </button>
+                  </div>
+               </div>
+            </div>
+         )}
+      </>
    );
 };
 
