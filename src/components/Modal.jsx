b7d344d9a077f1aca8bd078506eff5ada9c9cef7
Michelle Pettersson <156910491+Annamichelles@users.noreply.github.com>
2024-05-16 09:47:26 +0200

    merge commit

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -9,7 +9,7 @@ import StartPage from "./Startpage";
 /*import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';*/
 
-export default function Modal() {
+/*export default function Modal() {
    const [Modal, setModal] = useState(false);
 
    const toggleModal = () => {
@@ -29,7 +29,7 @@ export default function Modal() {
    return (
       /* <div onClick={toggleModal} className="xmark">
 <FontAwesomeIcon icon={faXmark} size="2x" />
-</div> put inside question card when questions is done*/
+</div> put inside question card when questions is done
       <>
          {Modal && (
             <div className="modal">
@@ -40,7 +40,7 @@ export default function Modal() {
                   </div>
                   {/* <div onClick={toggleModal} className="modal-xmark">
                      <FontAwesomeIcon icon={faXmark} size="2x" />
-                  </div> */}
+                  </div> }
                   <h3 className="modal-info">
                      Are you sure you want to exit the game? All your answers
                      will be lost.{" "}
@@ -59,4 +59,47 @@ export default function Modal() {
          )}
       </>
    );
-}
+}*/
+
+export default function Modal() {
+  const [Modal, setModal] = useState(false);
+
+
+   const toggleModal = () => {
+    setModal(!Modal);//! = true to false and false to true
+  };
+
+
+  const backToMenu = () => <StartPage />;
+
+
+   if(Modal) {
+    document.body.classList.add('active-modal')
+  } else {
+    document.body.classList.remove('active-modal')
+  }
+
+   const element = <FontAwesomeIcon icon={faXmark} />
+ 
+return (
+    <>
+
+    {Modal && (
+       <div className="modal">
+        <div onClick={toggleModal} className="overlay"></div>
+          <div className="popup-content">
+              <div onClick={toggleModal}>
+                  <FontAwesomeIcon icon={faXmark} size="2x" />
+              </div>
+              <h3 className="modal-info">Are you sure you want to exit the game? All your answers will be lost. </h3>
+              <div className="buttons">
+                  <button onClick={toggleModal} className="close-modal">No, go back</button>
+                  <button onClick={backToMenu} className="exit-btn"> Yes, exit</button>
+              </div>
+          </div>
+        </div>
+   )}
+   </>
+ )}
+
+   
