3831eaf7d45d701a34a584dd4071c80f7ea361d7
Michelle Pettersson <156910491+Annamichelles@users.noreply.github.com>
2024-05-16 14:03:18 +0200

    popup doneö

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -27,7 +27,7 @@ export default function Modal() {
         <div className="modal">
           <div onClick={toggleModal} className="overlay"></div>
           <div className="popup-content">
-            <div onClick={toggleModal}>
+            <div onClick={toggleModal} className="modal-xmark">
               <FontAwesomeIcon icon={faXmark} size="2x" />
             </div>
             <h3 className="modal-info">Are you sure you want to exit the game? All your answers will be lost. </h3>
@@ -41,57 +41,3 @@ export default function Modal() {
     </>
   )
 }
-
-
-
-/*export default function Modal() {
-   const [Modal, setModal] = useState(false);
-
-   const toggleModal = () => {
-      setModal(!Modal); //! = true to false and false to true
-   };
-
-   const backToMenu = () => <StartPage />;
-
-   if (Modal) {
-      document.body.classList.add("active-modal");
-   } else {
-      document.body.classList.remove("active-modal");
-   }
-
-   const element = <FontAwesomeIcon icon={faXmark} />;
-
-   return (
-      /* <div onClick={toggleModal} className="xmark">
-<FontAwesomeIcon icon={faXmark} size="2x" />
-</div> put inside question card when questions is done
-      <>
-         {Modal && (
-            <div className="modal">
-               <div onClick={toggleModal} className="overlay"></div>
-               <div className="popup-content">
-                  <div onClick={toggleModal} className="modal-xmark">
-                     <FontAwesomeIcon icon={faXmark} size="2x" />
-                  </div>
-                  {/* <div onClick={toggleModal} className="modal-xmark">
-                     <FontAwesomeIcon icon={faXmark} size="2x" />
-                  </div> }
-                  <h3 className="modal-info">
-                     Are you sure you want to exit the game? All your answers
-                     will be lost.{" "}
-                  </h3>
-                  <div className="buttons">
-                     <button onClick={toggleModal} className="close-modal">
-                        No, go back
-                     </button>
-                     <button onClick={backToMenu} className="exit-btn">
-                        {" "}
-                        Yes, exit
-                     </button>
-                  </div>
-               </div>
-            </div>
-         )}
-      </>
-   );
-}*/
