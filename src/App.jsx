4bccb7453e7aa2841d6518a0a3d3db75f8865187
Yui Jensen <kyanyui67@gmail.com>
2024-05-17 12:50:38 +0200

    commit
    I haven't changed any codes, it is just space

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,19 @@ import StartPage from "./components/Startpage.jsx";
 
 // Component Functional
 function App() {
-   // variables, each state is a variable kept in the component all the time
-   const [username, setUsername] = useState("");
+  // variables, each state is a variable kept in the component all the time
+  const [username, setUsername] = useState("");
 
-   useEffect(() => {
-      console.log(username);
-   }, []);
-   // dynamic html content
-   return (
-      <div className="App">
-         {username.length == 0 ? <Intro setName={setUsername} /> : ""}
-         {username.length > 0 ? <StartPage username={username} /> : ""}
-      </div>
-   );
+  useEffect(() => {
+    console.log(username);
+  }, []);
+  // dynamic html content
+  return (
+    <div className="App">
+      {username.length == 0 ? <Intro setName={setUsername} /> : ""}
+      {username.length > 0 ? <StartPage username={username} /> : ""}
+    </div>
+  );
 }
 
 export default App;
